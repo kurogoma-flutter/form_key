0c4a546556b85197928d60d0280f7f17a13d66ec
Kurogoma939 <kan.sss.939@gmail.com>
2022-10-04 02:37:03 +0900

    入力値をフォームのkeyで管理する

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,117 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Formをkeyで管理する'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Column(children: const [
+        SampleFormWidget(),
+      ]),
+    );
+  }
+}
+
+/// nameとemailを入力するフォーム
+///
+/// このフォームは、[GlobalKey]を使って、値を管理
+///
+/// [GlobalKey]を使うことで、フォームの値を取得できる
+///
+/// 送信ボタンを押したときに、フォームの値をデバッグで出力する
+class SampleFormWidget extends StatefulWidget {
+  const SampleFormWidget({super.key});
+
+  @override
+  State<SampleFormWidget> createState() => _SampleFormWidgetState();
+}
+
+class _SampleFormWidgetState extends State<SampleFormWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailKey = GlobalKey<FormFieldState>();
+  final _nameKey = GlobalKey<FormFieldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                key: _nameKey,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  border: OutlineInputBorder(), // 外枠付きデザイン
+                  filled: true, // fillColorで指定した色で塗り潰し
+                  fillColor: Colors.blue,
+                  labelText: "User Name",
+                  hintText: 'Enter your name',
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                key: _emailKey,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.email),
+                  border: OutlineInputBorder(), // 外枠付きデザイン
+                  filled: true, // fillColorで指定した色で塗り潰し
+                  fillColor: Colors.greenAccent,
+                  labelText: "Email address form",
+                  hintText: 'Enter your email',
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // 各Fieldのvalidatorを呼び出す
+                  if (_formKey.currentState!.validate()) {
+                    // 入力データが正常な場合の処理
+                    _formKey.currentState!.save();
+                  }
+                  // ValueKeyで指定した値を出力する
+                  debugPrint('email: ${_emailKey.currentState!.value}');
+                  debugPrint('name: ${_nameKey.currentState!.value}');
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
